7e643c5b1ce47b9bc4f9fc66720121df39824e20
Nidhish-07 <rch246chauhan@gmail.com>
2022-11-22 19:28:39 +0530

    Added deploy.js

--- smartContract/scripts/deploy.ts
+++ smartContract/scripts/deploy.ts
@@ -1,23 +1,27 @@
+import { hexStripZeros } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+async function main():Promise<void> {
+  
+  const Transactions=await ethers.getContractFactory("Transactions")
+  const transactions=await Transactions.deploy()
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await transactions.deployed()
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Transaction deployed to: ${transactions.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+async function runMain():Promise<void>{
+try {
+    await main()
+    process.exit(0)
+
+} catch (error) {
+console.log(error);
+process.exit(1)
+  
+}
+}
